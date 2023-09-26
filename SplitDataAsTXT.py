653a82150dc1885228178a08c933daba9e1bc75a
Lim JangHee <limjackie@naver.com>
2023-09-26 20:42:04 +0900

    CCTV Master

---
+++ SplitDataAsTXT.py
@@ -0,0 +1,43 @@
+import os
+import math
+
+###########################################################################
+# 데이터 셋 비율 조정
+# 세 변수의 합이 100이 되도록
+train_set_ratio = 70
+validation_set_ratio = 10
+test_set_ratio = 20
+############################################################################
+
+# file_path는 조정, 현 상태는 data/SplitDatAsTXT.py, data/images 안에 이미지들
+file_path = 'images\\'
+# 역슬래쉬가 아닌 /로 구분하기 위해 변경
+file_path_as_originSlash = file_path[:-1] + "/"
+
+file_names = os.listdir(file_path)
+file_len = len(file_names)
+i=0
+while i<file_len:
+    temp_fileName = file_names[i]
+    if temp_fileName[-4:] == ".txt":
+        file_names.pop(i)
+        file_len = len(file_names)
+        #print(str(i) + "   " + str(file_len) + "    " + temp_fileName)
+    else: i += 1
+
+# 1. Test Set 목록
+print("****************** 1. Train Set 목록(비율: " + str(train_set_ratio) + "%) ******************")
+for i in range(0, math.ceil(file_len * (train_set_ratio / 100))):
+    print('data/' + file_path_as_originSlash + file_names[i])
+final_index = math.ceil(file_len * (train_set_ratio / 100))
+
+# 2. Validation Set 목록
+print("****************** 2. Validation Set 목록(비율: " + str(validation_set_ratio) + "%) ******************")
+for i in range(final_index, final_index+ math.ceil(file_len * (validation_set_ratio / 100))):
+    print('data/' + file_path_as_originSlash + file_names[i])
+final_index = final_index + math.ceil(file_len * (validation_set_ratio / 100))
+
+# 3. Test Set 목록
+print("****************** 3. Test Set 목록(비율: " + str(test_set_ratio) + "%) ******************")
+for i in range(final_index, file_len):
+    print('data/' + file_path_as_originSlash + file_names[i])
